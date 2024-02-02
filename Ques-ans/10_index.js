e8033e9002432ea80e1d44931df20208cf195d3e
hafijur <hafijur0211@gmial.com>
2024-02-02 22:32:59 +0530

    update

--- Ques-ans/10_index.js
+++ Ques-ans/10_index.js
@@ -1,2 +1,14 @@
-// 10. write a javascript function which accepts a arguments and returns the type 
+// 10. write a javascript function which accepts a arguments and returns the type
+
 // Note: There are six possible value that typeof return: Object, function, number, string, and undefined
+
+function typeTeller(elem) {
+  return typeof elem;
+}
+
+console.log(typeTeller([]));
+console.log(typeTeller(12));
+console.log(typeTeller("23"));
+console.log(typeTeller(true));
+console.log(typeTeller(undefined));
+console.log(typeTeller(function() {}));
--- Ques-ans/15_index.js
+++ Ques-ans/15_index.js
@@ -1,2 +1,40 @@
 // Write a javascript function to clone an array
 
+// Method one
+
+// function cloneArr(arr) {
+//   return [...arr];
+// }
+
+// var newArr = cloneArr([1, 2, 3, 4]);
+// console.log(newArr);
+
+// Method two
+/*
+
+function cloneArr(arr) {
+    var newArr = [];
+    arr.forEach(function (e) {
+        newArr.push(e);
+    });
+    return newArr;
+}
+
+var clone = [1, 2, 3];
+console.log(clone);
+var newArr = cloneArr(clone);
+newArr.pop();
+console.log(newArr);
+
+*/
+
+// Method three
+
+function cloneArr(arr) {
+    return arr.map(function (e) {
+        return e;
+    });
+}
+
+var newArr = cloneArr([1, 2, 3, 4]);
+console.log(newArr);
