889856a5770afb5185f71018349b55f4797fa1b9
hafijur <hafijur0211@gmial.com>
2024-02-02 22:48:16 +0530

    question 16

---
+++ Ques-ans/16_index.js
@@ -0,0 +1,13 @@
+// Write a javascript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array.
+
+function retrieve(arr, n = 1) {
+    if (n <= arr.length) {
+        for (var i = 0; i < n; i++) {
+            console.log(arr[i]);
+        }
+    } else {
+        console.log("No elements");
+    }
+}
+
+retrieve([1, 2, 3, 4, 5], 5);
